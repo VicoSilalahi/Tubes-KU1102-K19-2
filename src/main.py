292488f3ee05b2c967c8bbc1a213c150fd92c1d7
VicoSilalahi <vicoco.lahi@gmail.com>
2023-10-23 12:58:51 +0700

    Fixed formatting and item quantity countdown

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ def initialize():
     return inventory, userList
 
 def displayMenu(inventory):
-    print("1. Input Credit\n2. Pilih Barang\n3. Ambil Kembalian dan Tutup Sesi")
+    print("1. Input Credit\n2. Pilih Item\n3. Ambil Kembalian dan Tutup Sesi")
     selection = int(input("Masukkan angka opsi: "))
     return selection
 
@@ -21,6 +21,7 @@ def inputCredit():
     if(credit<=0):
         print("Credit tidak valid!")
         return inputCredit()
+    clearScreen()
     return credit
 
 def showItem():
@@ -28,28 +29,36 @@ def showItem():
     for i in range(0, len(inventory[0])):
         print(f"{i+1}. {inventory[0][i]} p: {inventory[1][i]} q: {inventory[2][i]}", end="")
         if(inventory[2][i]==0):
-            print(" Habis!")
-        print()        
+            print(" Habis!", end="")
+        print()    
 
 def selItem(inventory, sum, credit, count, userList):
     showItem()
     selection = int(input("Nomor item yang ingin dibeli: "))
-    print(credit)
-    if(inventory[1][selection-1]<=credit):
-        sum = sum + inventory[1][selection-1]
-        count += 1
-        userList[count] = selection
+    # print(credit)
+    if(inventory[2][selection-1]>0):
+        if(inventory[1][selection-1]<=credit):
+            sum = sum + inventory[1][selection-1]
+            count += 1
+            userList[count] = selection
+            inventory[2][selection-1] -= 1
+            clearScreen()
+        else:
+            clearScreen()
+            print("Credit Kurang, pilih item lain, masukkan credit, atau tutup sesi dan ambil kembalian")
     else:
-        print("Credit Kurang, pilih barang lain, masukkan credit, atau tutup sesi dan ambil kembalian")
+        clearScreen()
+        print("Item Habis, pilih item lain")
     return sum, count, userList
 
 def checkActivity():
-    print("Lanjut sesi?\n1. Ya\n2. Tidak, tutup sesi, dan ambil kembalian")
+    print("Tutup Sesi?\n1. Ya, tutup sesi dan ambil kembalian\n2. Tidak, lanjut sesi")
     opt = int(input("Masukkan angka opsi: "))
     if(opt==1):
-        act = False
-    else:
         act = True
+    else:
+        act = False
+    clearScreen()
     return act
 
 def printReceipt(credit, userList):
@@ -58,11 +67,11 @@ def printReceipt(credit, userList):
     print(f"Item yang dibeli: ")
     while(check == True):
         i+=1
-        print(f"{i+1}. {inventory[0][userList[i]-1]}, ")
+        print(f"{i}. {inventory[0][userList[i]-1]}")
         if(userList[i+1]==0):
             check = False
     print(f"Total Pembelian : {sum}")
-    print(f"Kembalian Credit: {credit}")
+    print(f"Kembalian Credit: {credit}\n\n")
 
 while(True):
     print("Ini adalah vending machine minuman, silakan pilih menu")
@@ -73,7 +82,6 @@ while(True):
     Act = True
     count = 0
     while(Act == True):
-        clearScreen()
         print(f"Credit tersisa: {credit}")
         print(f"Total Pembelian: {sum}")
         selection = displayMenu(inventory)
