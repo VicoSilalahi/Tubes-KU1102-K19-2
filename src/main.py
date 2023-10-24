4a8f8724c6de4b880273361d27f44cd3013703b5
VicoSilalahi <vicoco.lahi@gmail.com>
2023-10-24 16:37:20 +0700

    Added Ascii Art and a few comments

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
+# Nama/NIM
+# Alvin Christopher Santausa/19623129
+# Anthony Alden Satriabudi/16523185
+# Kiyo Lee Tiono/16523003
+# Vico A C Silalahi/16523157
+
 # Deskripsi: Program Utama Tugas Besar Simulasi Vending Machine
 
+# Kamus Global
+# inventory, userList, credit, totalCred, sum, count, selection: Integer
+# Act: Boolean
+
+# Algoritma
+# Kumpulan Fungsi
+
+# Fungsi clearScreen()
+# Deskripsi: Fungsi untuk membuat newline sebanyak 50, membersihkan cli, bertindak seperti command 'cls' atau 'clear'
 def clearScreen():
     for i in range(50):
         print()
 
+# Fungsi initialize()
+# Deskripsi: Fungsi untuk menginisialisasi inventory, hanya dilakukan diawal ketika program pertama kali bekerja
 def initialize():
     inventory = [["Fanta", "Coca Cola", "Nescafe", "Aqua"], # Item at 0
                  [5000, 7000, 9000, 3000], # Price at 1
@@ -12,11 +29,15 @@ def initialize():
     userList = [0]*100
     return inventory, userList
 
+# Fungsi displayMenu()
+# Deskripsi: Fungsi untuk mengoutput pilihan menu kepada user
 def displayMenu(inventory):
-    print("1. Input Credit\n2. Pilih Item\n3. Ambil Kembalian dan Tutup Sesi")
+    print("1. Input Credit\n2. Pilih Item\n3. Ambil Kembalian dan Tutup Sesi\n4. Matikan Mesin")
     selection = int(input("Masukkan angka opsi: "))
     return selection
 
+# Fungsi inputCredit()
+# Deskrispi: Fungsi untuk menambah credit setelah pengisian credit awal
 def inputCredit():
     credit = int(input("Masukkan credit: "))
     if(credit<=0):
@@ -25,18 +46,21 @@ def inputCredit():
     clearScreen()
     return credit
 
+# Fungsi showItem()
+# Deskripsi: Fungsi untuk mengoutput item yang tersedia dengan harga dan kuantitas, serta status dari kuantitas tersebut
 def showItem():
     print("Item yang tesedia: ")
     for i in range(0, len(inventory[0])):
-        print(f"{i+1}. {inventory[0][i]} p: {inventory[1][i]} q: {inventory[2][i]}", end="")
+        print(f"{i+1}. {inventory[0][i]} Harga: {inventory[1][i]} Jumlah: {inventory[2][i]}", end="")
         if(inventory[2][i]==0):
-            print(" Habis!", end="")
+            print(", Habis!", end="")
         print()    
 
+# Fungsi selItem()
+# Deskripsi: Fungsi untuk memilih item yang ingin dibeli user, dengan memeriksa jumlah barang, serta sisa credit
 def selItem(inventory, sum, credit, count, userList):
     showItem()
     selection = int(input("Nomor item yang ingin dibeli: "))
-    # print(credit)
     if(inventory[2][selection-1]>0):
         if(inventory[1][selection-1]<=credit):
             sum = sum + inventory[1][selection-1]
@@ -52,6 +76,8 @@ def selItem(inventory, sum, credit, count, userList):
         print("Item Habis, pilih item lain")
     return sum, count, userList
 
+# Fungsi checkActivity()
+# Deskripsi: Fungsi untuk menutup atau melanjutkan sesi user
 def checkActivity():
     print("Tutup Sesi?\n1. Ya, tutup sesi dan ambil kembalian\n2. Tidak, lanjut sesi")
     opt = int(input("Masukkan angka opsi: "))
@@ -62,6 +88,8 @@ def checkActivity():
     clearScreen()
     return act
 
+# Fungsi printReceipt()
+# Deskripsi: Fungsi untuk mengoutput daftar item yang dibeli user, dan kembalian akhir
 def printReceipt(credit, userList):
     i = 0
     check = True
@@ -74,30 +102,56 @@ def printReceipt(credit, userList):
     print(f"Total Pembelian : {sum}")
     print(f"Kembalian Credit: {credit}\n\n")
 
+def printAscii():
+    ascii_art = '''
+     __   __           _ _             __  __         _    _          
+     \ \ / /__ _ _  __| (_)_ _  __ _  |  \/  |__ _ __| |_ (_)_ _  ___ 
+      \ V / -_) ' \/ _` | | ' \/ _` | | |\/| / _` / _| ' \| | ' \/ -_)
+       \_/\___|_||_\__,_|_|_||_\__, | |_|  |_\__,_\__|_||_|_|_||_\___|
+                               |___/                                  
+    '''
+    print(ascii_art)
+
+# Proses Utama
+
+# Inisialisasi Utama, menghidupkan mesin dan mengisi inventory
+state = True
 inventory, userList = initialize()
 
-while(True):
-    print("Ini adalah vending machine minuman, silakan pilih menu")
+# Program akan diloop hingga user/pemilik mesin mematikan mesin
+while(state == 1):
+    printAscii()
     credit = inputCredit()
     totalCred = credit
     sum = 0
     Act = True
     count = 0
     while(Act == True):
+        printAscii()
         print(f"Credit tersisa: {credit}")
         print(f"Total Pembelian: {sum}")
         selection = displayMenu(inventory)
         if(selection == 1):
             clearScreen()
+            printAscii()
             totalCred = totalCred + inputCredit()
             credit = totalCred - sum 
         elif(selection == 2):
             clearScreen()
+            printAscii()
             print("Credit tersisa: ", credit)
             sum, count, userList = selItem(inventory, sum, credit, count, userList)
             credit = totalCred - sum
         elif(selection == 3):
             clearScreen()
+            printAscii()
             if(checkActivity()):
                 printReceipt(credit, userList)
                 Act = False
+        elif(selection == 4):
+            clearScreen()
+            printAscii()
+            onoff = int(input("Matikan Mesin?\n1. Ya\n2. Tidak"))
+            if(onoff==1):
+                Act = False
+                state = False
